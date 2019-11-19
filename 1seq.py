75956aaa98e4d3e139c27558efb20c325754d6cf
sneggorod2019 <sneggorod@mail.ru>
2019-11-19 22:41:50 +0300

    добавил в первое задание сортировку

--- 1seq.py
+++ 1seq.py
@@ -4,4 +4,5 @@ nomer_element = 1
 for i in range(n):
     new_element = int(input('Введите  элемент:{0} '.format(i+1))) # считываем очередной элемент
     a.append(new_element)  # добавляем его в список
+    a.sort()
 print(a)
